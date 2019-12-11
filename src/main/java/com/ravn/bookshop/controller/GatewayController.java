e0f231839b3b9693fab2eb67ff3a95a02d39d4c6
endikap <endika.posadas@ravn.co.uk>
2019-12-11 10:36:52 +0000

    add comments rest

--- src/main/java/com/ravn/bookshop/controller/GatewayController.java
+++ src/main/java/com/ravn/bookshop/controller/GatewayController.java
@@ -6,10 +6,7 @@ import com.ravn.bookshop.model.Comment;
 import com.ravn.bookshop.model.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -25,7 +22,7 @@ public class GatewayController {
 
     @RequestMapping("/books")
     @CrossOrigin
-    public List<Book> getAll(){
+    public List<Book> allBooks(){
         final List<Comment> comments1 = Arrays.asList(Comment.builder().author("me").id("1").text("great!").build(),
                 Comment.builder().author("you").id("2").text("horrible").build());
         final List<Review> reviews = Collections.singletonList(Review.builder().author("me").id("34").likes(99).text("best book").comments(comments1).build())  ;
@@ -35,8 +32,17 @@ public class GatewayController {
                 Book.builder().title("day").author("author").publishDate("2018-01-21").isbn("283").reviews(Collections.emptyList()).build());
     }
 
+
     @RequestMapping("/books/{id}/reviews")
-    public List<Review> getAll(@PathVariable String id) {
+    public List<Review> reviewsForBook(@PathVariable String id) {
         return reviewClient.getReviews("123");
     }
+
+
+    @PostMapping("/review/{id}/comment")
+    public Comment commentForReview(@PathVariable String id, Comment comment) {
+        return comment;
+    }
+
+
 }
