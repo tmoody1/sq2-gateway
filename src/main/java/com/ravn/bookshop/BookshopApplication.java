5e602cba1003d40272115a2b45c82fd6008d5efc
jamesa <james.ashbourne@imanage.com>
2019-12-11 09:50:43 +0000

    adding cors config

--- src/main/java/com/ravn/bookshop/BookshopApplication.java
+++ src/main/java/com/ravn/bookshop/BookshopApplication.java
@@ -2,12 +2,23 @@ package com.ravn.bookshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BookshopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookshopApplication.class, args);
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
 }
